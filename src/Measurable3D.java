1072db49305732e36164ef8353f492ebb41e157f
mr-berrett <rberrett@lwsd.org>
2022-03-28 20:34:30 -0700

    update comment

--- src/Measurable3D.java
+++ src/Measurable3D.java
@@ -1,5 +1,5 @@
-// implementing this interface indicates the class represents a 3D
-// object that has both measurable volume and surface area
+// implementing this interface indicates that a class represents a 3D 
+// object that has both measurable volume and measurable surface area
 public interface Measurable3D {
     public double getSurfaceArea();
     public double getVolume();
